f7a756cb1f3c3781dca21b591d0288f9eb2ac565
Tawnee Harris <tawneeh@icloud.com>
2020-11-03 21:43:53 -0800

    add groceries project

---
+++ README.md
@@ -0,0 +1,34 @@
+# Grocery List
+
+#### This application will organize a grocery list for the user. 10/30/2020
+
+#### By: Tawnee Harris
+
+## Description
+
+This application will take user input from a form and display the listed items into an alpahbetized, uppercased grocery list. 
+
+## Setup/Installation Requirements
+
+
+## Known Bugs
+
+There are no known bugs.
+
+## Support and contact details
+
+Feel free to contact me at <tawneeh@icloud.com>
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+* jQuery
+
+### License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Copyright (c) 2020 Tawnee Harris
---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function makeList() {
+  let groceryItemNames = ["#itemOne", "#itemTwo", "#itemThree", "#itemFour", "#itemFive"]  
+  let groceryList = [];
+  for (let i= 0; i < groceryItemNames.length; i++) {
+    groceryList.push(getItemValue(groceryItemNames[i]));
+  }
+  return groceryList.sort();
+}
+
+
+function getItemValue(itemName) {
+  let itemValue = $(itemName).val();
+  itemValue = itemValue.toUpperCase();
+  return itemValue;
+}
+
+$(document).ready(function() {
+  $("#groceries").submit(function(event) {
+
+  $("#list").append(`<li>${makeList()}</li>`);
+
+  event.preventDefault();
+
+  });
+});
+
+
+
+//groceryList.forEach(makeList()) {}
+// when the user submits the form, hide the form and display the list items
+// make a new array w/ the list alphabetical w/ .sort(), uppercased inside a <ul>
